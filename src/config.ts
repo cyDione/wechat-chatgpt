56340e54a0f608ce27f9232fd93267a46b47552c
CyDione <45588391+cyDione@users.noreply.github.com>
2024-04-07 11:56:36 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ import { IConfig } from "./interface";
 export const config: IConfig = {
   api: process.env.API,
   openai_api_key: process.env.OPENAI_API_KEY || "123456789",
-  model: process.env.MODEL || "gpt-3.5-turbo",
+  model: process.env.MODEL || "gpt-4",
   chatPrivateTriggerKeyword: process.env.CHAT_PRIVATE_TRIGGER_KEYWORD || "",
   chatTriggerRule: process.env.CHAT_TRIGGER_RULE || "",
   disableGroupMessage: process.env.DISABLE_GROUP_MESSAGE === "true",
